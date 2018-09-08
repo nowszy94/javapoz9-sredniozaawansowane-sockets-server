f5238f08ed097230e8d5c3ef7285dd632e021d60
Szymon Nowak <nowszy94@gmai.com>
2018-09-08 14:12:53 +0200

    sockets example

--- src/main/java/com/sda/Application.java
+++ src/main/java/com/sda/Application.java
@@ -1,7 +1,24 @@
 package com.sda;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8081);
+        boolean b = true;
+        while (b) {
+            try {
+                Socket socket = serverSocket.accept();
+                System.out.println(socket.getInetAddress());
+                ServerTask serverTask = new ServerTask(socket);
+                new Thread(serverTask).start();
+            } catch (Exception e) {
+                //consume
+            }
+        }
+        serverSocket.close();
     }
 }
